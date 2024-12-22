941debba432bce410d2a216dfa3dfdfd8ffe9b09
Ray Forman <rayforman150@gmail.com>
2024-12-22 17:00:49 -0500

    file cleanup

--- server/config.js
+++ server/config.js
@@ -3,6 +3,5 @@ require('dotenv').config();
 module.exports = {
   spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
   spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  openWeatherApiKey: process.env.OPENWEATHER_API_KEY,
   redirectUri: process.env.REDIRECT_URI || 'http://localhost:5001/auth/callback'
 };
